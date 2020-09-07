b52b47469464d4d8df49bebe5babb6154271a38b
Tanzeel <35539906+dw44@users.noreply.github.com>
2020-09-08 04:51:51 +0500

    created user model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true 
+  },
+  lastName: { 
+    type: String, 
+    required: true 
+  },
+  username: { 
+    type: String, 
+    required: true 
+  },
+  password: {
+    type: String,
+    required: true
+  } 
+});
+
+UserSchema.virtual('fullName').get(function () {
+  return `${this.lastName}, ${this.firstName}`;
+});
+
+module.exports = mongoose.model('User', UserSchema);
