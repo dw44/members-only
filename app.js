ee11f7e7db8e3b9479ee2a75c3933dcf545e21bb
Tanzeel <35539906+dw44@users.noreply.github.com>
2020-09-08 02:20:04 +0500

    updated boilerplate

--- app.js
+++ app.js
@@ -4,18 +4,51 @@ const path = require('path');
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const mongoose = require("mongoose");
 const expressLayouts = require('express-ejs-layouts');
+const connectDB = require('./config/db');
 
 dotenv.config({path: './config/config.env'});
-const Schema = mongoose.Schema;
-
 const app = express();
+connectDB();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({usename: username}, (err, user) => {
+      if (err) {
+        return done(err);
+      };
+      if (!user) {
+        return done(null, false, { msg: 'Incorrect Username' });
+      };
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) return done(null, user);
+        else return done(null, false, { msg: 'Incorrect Password' });
+      });
+    });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
 app.use(expressLayouts);
+app.use(session({secret: process.env.secret, resave: false, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+});
 
 app.get('/', (req, res) => {
     res.send('Work in progress!');
